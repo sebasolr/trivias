f4b5692888fd1d3430928431354ba47a4e98278e
Sebastian Liberona <sebasolr.it@gmail.com>
2022-09-04 16:53:02 -0300

    method authetication aplicate

--- scr/lib/passport.js
+++ scr/lib/passport.js
@@ -1,10 +1,54 @@
 const passport = require( 'passport');
 const LocalStrategy = require( 'passport-local').Strategy
 
-passport.use('local.signup', new LocalStrategy({
-    usernameField: 'email',
+const pool = require('../database')
+const helper = require('../lib/helpers')
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'username',
     passwordField: 'password',
     passReqToCallback: true
-}, async (req,username,pass,done) =>{
-    console.log(req.body);
+}, async (req,username, password,done) =>{
+   const rows= await pool.query(`select * from usuarios where username = ?`[username])
+    if (rows.length > 0) {
+        const user = rows[0];
+        const validando = await helper.matchPassword(password, user.password)
+        if(validando) {
+            done(null,user,req.flash('Bienvenido'+  username));}
+            else {
+                done(null,false,req.flash('Clave incorrecta'));
+            }}
+    else{
+        return done(null,false,req.flash('El username no es correcto'))
+    }
+
+})) 
+
+
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req,username,password,done) =>{
+    const { firstname,lastname,email }=req.body;
+    const newUser = {
+        firstname,
+        lastname,
+        email,
+        username,
+        password
+    };
+    newUser.password =  await helper.encryptPassword(password)
+    const result = await pool.query('INSERT INTO usuarios SET ?', [newUser])
+    newUser.id = result.insertId;
+    return done(null,newUser)
 }));
+
+passport.serializeUser((user,done) =>{
+    done(null,user.id);
+});
+
+passport.deserializeUser(async (id,done) =>{
+    pool.query ('SELECT * FROM usuarios where id = ?',[id]);
+    done(null, id);
+});
--- scr/routes/authetication.js
+++ scr/routes/authetication.js
@@ -3,15 +3,27 @@ const router = express.Router();
 
 const passport = require('passport');
 
+//crear usuarios para la aplicacion
 router.get('/signup', (req, res) => {
-    res.render('links/login');})
+    res.render('auth/login');})
+
 
 router.post('/signup', passport.authenticate('local.signup',{
-        successRedirect: '/profile',
+        successRedirect: '/signin',
         failureRedirect: '/signup',
         failureFlash: true
 }));
 
+//logear a los usuarios
+router.get('/signin',(req, res) => {    
+        res.render('auth/login');
+})
+router.post('/signin', (req, res,next) => {
+        passport.authenticate('local.signin',{
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+})(req,res,next)});
 
 
 router.get('/profile', (req, res) => {
